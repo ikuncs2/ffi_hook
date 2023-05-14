bcacc56e0b9f997b77d8b83d78b2d87f8bac2fe0
MoeMod Server #2 <824395314@qq.com>
2023-05-15 00:48:49 +0800

    update for luajit ffi

--- src/luaopen_ffi_hook.cpp
+++ src/luaopen_ffi_hook.cpp
@@ -1,4 +1,9 @@
-#include <lua.hpp>
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
 #include "inline.h"
 #include <memory>
 //#include <base/hook/iat.h>
@@ -22,34 +27,18 @@ struct hook_t {
 	base::hook::hook_t hi = 0;
 };
 
+int FFI_OBJ;
+
 static int push_funcdef(lua_State* L, int idx) {
 	switch (lua_type(L, idx)) {
-		case LUA_TSTRING:
-			lua_pushvalue(L, idx);
-			return 1;
-		case LUA_TFUNCTION: {
-			if (!lua_getupvalue(L, idx, 1)) {
-				return luaL_argerror(L, idx, "has no upvalue");
-			}
-			if (lua_type(L, -1) != LUA_TUSERDATA) {
-				return luaL_argerror(L, idx, lua_pushfstring(L, "bad upvalue #1, userdata expected, got %s", luaL_typename(L, -1)));
-			}
-			size_t fdef_len = 0;
-			const char* fdef = luaL_tolstring(L, -1, &fdef_len);
-			if (!fdef || fdef_len <= 7) {
-				return luaL_argerror(L, idx, lua_pushfstring(L, "bad upvalue #1, userdata expected, got %s", luaL_typename(L, -1)));
-			}
-			lua_pushlstring(L, fdef + 6, fdef_len - 7);
-			lua_remove(L, -2);
-			return 1;
-		}
 		case LUA_TUSERDATA: {
-			size_t fdef_len = 0;
-			const char* fdef = luaL_tolstring(L, idx, &fdef_len);
-			if (!fdef || fdef_len <= 7) {
-				return luaL_argerror(L, idx, lua_pushfstring(L, "bad userdata #%d, can't get define", idx));
-			}
-			lua_pushlstring(L, fdef + 6, fdef_len - 7);
+			lua_pushvalue(L, idx); // 1 = obj
+			lua_pushlightuserdata(L, &FFI_OBJ);
+			lua_rawget(L, LUA_REGISTRYINDEX); // 2 = ffi
+			lua_getfield(L, -1, "typeof"); // 3 = ffi.typeof
+			lua_insert(L, -3); // ffi.typeof, obj, ffi
+			lua_pop(L, 1);
+			lua_call(L, 1, 1); // ffi.typeof(obj)
 			return 1;
 		}
 		default:
@@ -58,13 +47,22 @@ static int push_funcdef(lua_State* L, int idx) {
 }
 
 static int hook_call(lua_State* L) {
-	luaL_testudata(L, 1, "hook");
-	if (LUA_TTABLE != lua_getuservalue(L, 1)) {
+	hook_t* h = (hook_t*)lua_touserdata(L, 1);
+
+	lua_getfenv(L, 1);
+	if (LUA_TTABLE != lua_type(L, -1)) {
 		return 0;
 	}
-	if (LUA_TUSERDATA != lua_geti(L, -1, 1)) {
-		return 0;
-	}
+
+	lua_pushlightuserdata(L, &FFI_OBJ);
+	lua_rawget(L, LUA_REGISTRYINDEX);
+	lua_getfield(L, -1, "cast");
+	lua_remove(L, -2); // ffi.cast
+
+	lua_rawgeti(L, -2, 1);
+	lua_pushlightuserdata(L, (void *)h->real);
+	lua_call(L, 2, 1); // ffi.cast(cdef, h.real)
+
 	lua_remove(L, -2);
 	lua_replace(L, 1);
 	lua_call(L, lua_gettop(L) - 1, LUA_MULTRET);
@@ -72,7 +70,7 @@ static int hook_call(lua_State* L) {
 }
 
 static int hook_remove(lua_State* L) {
-	hook_t* h = (hook_t*)luaL_testudata(L, 1, "hook");
+	hook_t* h = (hook_t*)lua_touserdata(L, 1); // "hook"
 	if (!h) {
 		return 0;
 	}
@@ -84,148 +82,107 @@ static int hook_remove(lua_State* L) {
 	return 0;
 }
 
-int FFI_NEW;
-
-static int hookInitialize(lua_State* L) {
-	luaL_checktype(L, 1, LUA_TFUNCTION);
+static int hookSetFFI(lua_State* L) {
+	luaL_checktype(L, 1, LUA_TTABLE);
 	lua_settop(L, 1);
-	lua_rawsetp(L, lua_upvalueindex(1), &FFI_NEW);
+	lua_pushlightuserdata(L, &FFI_OBJ);
+	lua_insert(L, -2);
+	lua_rawset(L, LUA_REGISTRYINDEX);
 	return 0;
 }
 
-static void infInline(lua_State* L, hook_t* h) {
+typedef void(*infHook)(lua_State* L, hook_t* h);
+
+static void initHookStruct(hook_t *h, void *old_addr, void *new_addr)
+{
 	h->type = hook_t::type::Inline;
-	lua_getupvalue(L, 1, 1);
-	h->real = *(uintptr_t*)((uintptr_t)lua_touserdata(L, -1) + 16);
-	lua_pop(L, 1);
+	h->fake = (uintptr_t)new_addr;
+	h->real = (uintptr_t)old_addr; // old func addr
 	base::hook::install(&h->real, h->fake, &h->hi);
 }
 
-//static void infIAT(lua_State* L, hook_t* h) {
-//	h->type = hook_t::type::IAT;
-//	const char* dllname = luaL_checkstring(L, 3);
-//	const char* apiname = nullptr;
-//	if (lua_isinteger(L, 4)) {
-//		apiname = (const char*)luaL_checkinteger(L, 4);
-//	}
-//	else {
-//		apiname = luaL_checkstring(L, 4);
-//	}
-//	if (lua_isinteger(L, 2)) {
-//		h->real = base::hook::iat((HMODULE)luaL_checkinteger(L, 2), dllname, apiname, h->fake);
-//	}
-//	else {
-//		std::wstring modulename = lua_towstring(L, 2);
-//		h->real = base::hook::iat(modulename.c_str(), dllname, apiname, h->fake);
-//	}
-//}
+// function inline(old_func : userdata, callback : function)
+static int hookInline(lua_State* L) {
+	luaL_checktype(L, 1, LUA_TUSERDATA);
+	luaL_checktype(L, 2, LUA_TFUNCTION);
+	lua_settop(L, 2);
+	push_funcdef(L, 1); // 3=cdef=ffi.type(old_func)
+	lua_pushvalue(L, 2); // 4 = callback
 
-typedef void(*infHook)(lua_State* L, hook_t* h);
-
-static int hookInterface(lua_State* L, infHook ih) {
-	lua_rawgetp(L, lua_upvalueindex(1), &FFI_NEW);
+	lua_pushlightuserdata(L, &FFI_OBJ);
+	lua_rawget(L, LUA_REGISTRYINDEX);
+	lua_getfield(L, -1, "new");
+	lua_remove(L, -2); // 5=ffi.new
 	if (lua_type(L, -1) != LUA_TFUNCTION) {
 		return luaL_error(L, "No initialization.");
 	}
 
-	lua_pushvalue(L, -1);
-	lua_pushvalue(L, -4);
-	lua_pushnil(L);
-	lua_call(L, 2, 1);
+	lua_pushvalue(L, -3); // old_func, callback, cdef, callback, ffi.new, cdef
 
-	lua_insert(L, -4);
-	lua_insert(L, -3);
-	lua_call(L, 2, 1);
+	lua_insert(L, -4); // old_func, callback, cdef, cdef, callback, ffi.new
+	lua_insert(L, -3); // old_func, callback, cdef, ffi.new, cdef, callback
+	lua_call(L, 2, 1); // old_func, callback, cdef, ffi.new(cdef, callback)
 
-	hook_t* h = (hook_t*)lua_newuserdata(L, sizeof hook_t);
+	hook_t* h = (hook_t*)lua_newuserdata(L, sizeof hook_t); // 5=hook
 	if (!h) {
 		return 0;
 	}
 	new (h) hook_t;
-	luaL_setmetatable(L, "hook");
-	lua_newtable(L);
-	lua_pushvalue(L, -4);
-	lua_seti(L, -2, 1);
-	lua_pushvalue(L, -3);
-	lua_seti(L, -2, 2);
-	lua_setuservalue(L, -2);
+	luaL_getmetatable(L, "hook");
+	lua_setmetatable(L, -2);
 
-	h->fake = *(uintptr_t*)((uintptr_t)lua_touserdata(L, -2) + 16);
-	ih(L, h);
-	uintptr_t cd = (uintptr_t)lua_touserdata(L, -3);
-	*(uintptr_t*)(cd + 16) = h->real;
-	return 1;
+	lua_newtable(L); // 6=hook data
+	lua_pushvalue(L, -4); // 7=cdef
+	lua_rawseti(L, -2, 1);
+	lua_pushvalue(L, -3); // 7=ffi.new(cdef, callback)
+	lua_rawseti(L, -2, 2);
+	lua_setfenv(L, -2); // 3=cdef, 4=ffi.new(cdef, callback), 5=hook
+
+	lua_pushlightuserdata(L, &FFI_OBJ);
+	lua_rawget(L, LUA_REGISTRYINDEX); // 6=ffi
+	lua_getfield(L, -1, "cast");
+	lua_remove(L, -2); // 6=ffi.cast
+
+	lua_pushstring(L, "void (*)(void *, void *, void *)");
+	lua_pushlightuserdata(L, (void*)initHookStruct);
+	lua_call(L, 2, 1); // 6=ffi.cast("void (*)(void *, void *, void *)", initHookStruct)
+
+	lua_pushvalue(L, -2); // 7=hook
+	lua_pushvalue(L, 1); // 8=old_func cdata
+	lua_pushvalue(L, -5); // 9=ffi.new(cdef, callback)
+	lua_call(L, 3, 0); // 5=hook
+
+	return 1; // return hook
 }
 
-static int hookInline(lua_State* L) {
-	luaL_checktype(L, 1, LUA_TFUNCTION);
-	luaL_checktype(L, 2, LUA_TFUNCTION);
-	lua_settop(L, 2);
-	push_funcdef(L, 1);
-	lua_pushvalue(L, 2);
-	return hookInterface(L, infInline);
-}
-
-//static int hookIAT(lua_State* L) {
-//	luaL_checktype(L, 5, LUA_TFUNCTION);
-//	lua_settop(L, 5);
-//	push_funcdef(L, 1);
-//	lua_pushvalue(L, 5);
-//	return hookInterface(L, infIAT);
-//}
-
-static int hookC2lua(lua_State* L) {
-	luaL_checktype(L, 1, LUA_TSTRING);
-	luaL_checktype(L, 2, LUA_TNUMBER);
-	lua_rawgetp(L, lua_upvalueindex(1), &FFI_NEW);
-	if (lua_type(L, -1) != LUA_TFUNCTION) {
-		return luaL_error(L, "No initialization.");
+// from lua 5.3
+static void luaL_setfuncs(lua_State* L, const luaL_Reg* l, int nup) {
+	luaL_checkstack(L, nup + 1, "too many upvalues");
+	for (; l->name != NULL; l++) {  /* fill the table with given functions */
+		int i;
+		lua_pushstring(L, l->name);
+		for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+			lua_pushvalue(L, -(nup + 1));
+		lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+		lua_settable(L, -(nup + 3)); /* table must be below the upvalues, the name and the closure */
 	}
-	uintptr_t fp = (uintptr_t)luaL_checkinteger(L, 2);
-	lua_pushvalue(L, 1);
-	lua_pushnil(L);
-	lua_call(L, 2, 1);
-	uintptr_t cd = (uintptr_t)lua_touserdata(L, -1);
-	*(uintptr_t*)(cd + 16) = fp;
-	return 1;
-}
-
-static int hookLua2c(lua_State* L) {
-	luaL_checktype(L, 1, LUA_TSTRING);
-	lua_rawgetp(L, lua_upvalueindex(1), &FFI_NEW);
-	if (lua_type(L, -1) != LUA_TFUNCTION) {
-		return luaL_error(L, "No initialization.");
-	}
-	lua_pushvalue(L, 1);
-	lua_pushvalue(L, 2);
-	lua_call(L, 2, 1);
-	uintptr_t cd = (uintptr_t)lua_touserdata(L, -1);
-	lua_pushinteger(L, *(uintptr_t*)(cd + 16));
-	return 1;
+	lua_pop(L, nup);  /* remove upvalues */
 }
 
 extern "C" __declspec(dllexport)
-int luaopen_hook(lua_State* L)
+int luaopen_ffi_hook(lua_State* L)
 {
-	static luaL_Reg mt[] = {
-		{ "remove", hook_remove },
-		{ "__call", hook_call },
-		{ NULL, NULL },
-	};
 	luaL_newmetatable(L, "hook");
-	luaL_setfuncs(L, mt, 0);
+	lua_pushcfunction(L, hook_remove);
+	lua_setfield(L, -2, "remove");
+	lua_pushcfunction(L, hook_call);
+	lua_setfield(L, -2, "__call");
 
-	static luaL_Reg lib[] = {
-		{ "initialize", hookInitialize },
-		{ "inline", hookInline },
-		//{ "iat", hookIAT },
-		{ "c2lua", hookC2lua },
-		{ "lua2c", hookLua2c },
-		{ NULL, NULL }
-	};
-	luaL_newlibtable(L, lib);
-	lua_pushvalue(L, -1);
-	luaL_setfuncs(L, lib, 1);
+	lua_newtable(L);
+	lua_pushcfunction(L, hookSetFFI);
+	lua_setfield(L, -2, "setffi");
+	lua_pushcfunction(L, hookInline);
+	lua_setfield(L, -2, "inline");
 	return 1;
 }
 
